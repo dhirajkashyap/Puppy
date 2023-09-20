58c8976ca887fba45400f810323ecaa7b5e8c7ce
dhirajkashyap <65522950+dhirajkashyap@users.noreply.github.com>
2023-09-20 15:30:29 +0000

    Trial errors

--- Puppy.go
+++ Puppy.go
@@ -1,9 +1,23 @@
 package puppy
 
+import (
+	"github.com/dhirajkashyap/Dog"
+)
+
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof!"
+}
+
+func Barkz() string {
+	return "Woof! Woof! Woof!"
+}
+
+
+func BigBark() string {
+	return dog.WhenGrownUp(Barkz()))
 }
